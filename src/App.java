d8f11eb070662c0d41ef9fb64f7ce2b49cc2d4b3
Kajen <ludvigda@chalmers.se>
2023-12-07 09:14:55 +0100

    Changes by emil

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
 public class App {
     public static void main(String[] args) {
         CarController cc = new CarController();
+        addObserverMethod(cc);
+    }
+
+    private static void addObserverMethod(CarController cc){
         cc.model.timerListener.addObserver(cc.frame.drawPanel);
     }
 }
